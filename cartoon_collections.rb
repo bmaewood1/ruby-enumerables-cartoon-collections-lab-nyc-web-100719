ec5be1539b3fe5fe5e6f7b537d3a15da438a0152
Bailey Wood <woodbmae@gmail.com>
2019-09-24 18:04:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,9 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.include?(cheese_types)
+  array.find do |type|
+    cheese_types.include?(type)
+  end
 end
 
 
